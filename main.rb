6866da711d5c0319c98e040ef0fa1f3b4f9f627c
iiom <ioma766@gmail.com>
2019-02-05 16:49:12 +0300

    edit main

--- main.rb
+++ main.rb
@@ -5,9 +5,10 @@ require_relative 'lib/stock'
 
 puts '---------Table------------'
 arr_tables = []
-def tables(arr_tables, arg1, arg2, status)
-  arr_tables << Table.new(arg1, arg2, status)
+def tables(arr_tables, id_table, seats_number, status)
+  arr_tables << Table.new(id_table, seats_number, status)
 end
+
 tables(arr_tables, 1,2, 'free')
 tables(arr_tables, 2,4, 'free')
 tables(arr_tables, 3,6, 'free')
