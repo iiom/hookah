4969de37060bce94c6991da56e41375d6cdd22df
iiom <ioma766@gmail.com>
2019-02-05 15:37:41 +0300

    edit main

--- main.rb
+++ main.rb
@@ -1,3 +1,20 @@
 require_relative 'lib/table'
-
+require_relative 'lib/hookah'
+require_relative 'lib/bowl'
+puts '---------Table------------'
 p Table.new(1, 4, 'free')
+p Table.new(1, 2, 'free')
+p Table.new(1, 8, 'free')
+p Table.new(1, 4, 'free')
+p Table.new(1, 2, 'free')
+puts '--------HOOKAH------------'
+p Hookah.new('Khalil Mamoon')
+p Hookah.new('MEDUSE')
+p Hookah.new('FUMO')
+p Hookah.new('Fabula')
+p Hookah.new('Syrian')
+puts '---------BOWL-------------'
+p Bowl.new('earthenware')
+p Bowl.new('ceramic')
+p Bowl.new('vortex')
+p Bowl.new('silicone')
