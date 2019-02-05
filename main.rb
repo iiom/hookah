96eb15b6053616ef63d58db4b13cc13047ea15f4
iiom <ioma766@gmail.com>
2019-02-05 17:30:49 +0300

    edit main

--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ require_relative 'lib/table'
 require_relative 'lib/hookah'
 require_relative 'lib/bowl'
 require_relative 'lib/stock'
+require_relative 'lib/tobacco'
 
-puts '---------Table------------'
 arr_tables = []
 def tables(arr_tables, id_table, seats_number, status)
   arr_tables << Table.new(id_table, seats_number, status)
@@ -14,9 +14,7 @@ tables(arr_tables, 2,4, 'free')
 tables(arr_tables, 3,6, 'free')
 tables(arr_tables, 4,4, 'free')
 tables(arr_tables, 5,6, 'free')
-puts arr_tables
 
-puts '--------STOCk------------'
 stock = Stock.new
 stock.add_charcoals(900)
 stock.hookahs('Khalil Mamoon')
@@ -29,8 +27,16 @@ stock.bowls('Phunnel')
 stock.bowls('Ceramic')
 stock.bowls('Vortex')
 stock.bowls('Silicon')
+stock.tobacco('Adaliya',200 , 500)
+stock.tobacco('Al_Fakher',220 , 800)
+stock.tobacco('D_mini',160 , 550)
+stock.tobacco('Dark_Side',260 , 900)
+stock.tobacco('Dokha',220 , 650)
+stock.tobacco('Nakhla',180 , 600)
+stock.tobacco('WTO',200 , 700)
 
-p stock.arr_hookah
-p stock.arr_bowl
-p stock.arr_tobacco
-p stock.charcoals
+puts "amount of tables - #{arr_tables.size}"
+puts "hookahs - #{stock.arr_hookah.size}"
+puts "bowls - #{stock.arr_bowl.size}"
+puts "amount of tobacco - #{stock.arr_tobacco.size}"
+puts "amount of charcoals - #{stock.charcoals}"
