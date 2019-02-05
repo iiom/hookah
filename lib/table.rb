b8b563fb728c92c9044adbaeb1803d6d364ab75d
iiom <ioma766@gmail.com>
2019-02-05 15:15:14 +0300

    edit table

--- lib/table.rb
+++ lib/table.rb
@@ -1,3 +1,10 @@
 class Table
+  attr_reader :id_table, :seats_number
+  attr_accessor :status
 
+  def initialize(arg1, arg2, status)
+    @id_table     = arg1
+    @seats_number = arg2
+    @status       = status
+  end
 end
---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative 'lib/table'
+
+p Table.new(1, 4, 'free')
