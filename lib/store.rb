5f98ed881dd86daa1f32015a1301daebfe074c66
iiom <ioma766@gmail.com>
2019-02-05 15:47:37 +0300

    edit store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store
+  attr_accessor :wto, :dokha, :d_mini, :adalya, :nakhla, :al_fakher, :dark_side, :charcoal
 
+  def initialize(adalya = 200, al_fakher = 220, d_mini = 180, dark_side = 240, dokha = 160, nakhla = 200, wto = 220, charcoal = 900)
+    @wto       = wto
+    @dokha     = dokha
+    @adalya    = adalya
+    @d_mini    = d_mini
+    @nakhla    = nakhla
+    @al_fakher = al_fakher
+    @dark_side = dark_side
+    @charcoal  = charcoal
+  end
 end
