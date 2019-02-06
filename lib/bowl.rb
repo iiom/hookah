cfe3c39a6fec708642b6170ecb9bdadbf0abd671
iiom <ioma766@gmail.com>
2019-02-06 13:59:21 +0300

    edit bowl

--- lib/bowl.rb
+++ lib/bowl.rb
@@ -1,8 +1,9 @@
 class Bowl
-  attr_reader :type
+  attr_reader :type, :id
   attr_accessor :status
 
-  def initialize(type)
+  def initialize(type, id)
+    @id = id
     @type = type
     @status = 'free'
   end
