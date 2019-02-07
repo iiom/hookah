a0026369c159df6f3133f7ddde04d395ae40941d
iiom <ioma766@gmail.com>
2019-02-07 13:53:34 +0300

    refactor order

--- lib/order.rb
+++ lib/order.rb
@@ -9,4 +9,65 @@ class Order
     @hookah = []
     @tobacco = []
   end
+
+  def reserv_table(arr_tables, id)
+    tmp = []
+    arr_tables.select {|elem| elem.id == id}.first.status = 'reserv'
+    tmp << arr_tables.select {|elem| elem.id == id}
+    puts "no more free table" if show_id(arr_tables, 'free').size == 0
+    tmp.flatten!
+  end
+
+  def unreserv_table(arr_tables, id)
+    arr_tables.select {|elem| elem.id == id}.first.status = 'free'
+  end
+
+  def reserv_hookah(reserved_hookahs, stock, name_hookah = nil, id = nil)
+    tmp = []
+    id = stock.arr_hookahs.sample.id if name_hookah == nil && id == nil
+    id = stock.arr_hookahs.select {|i| i.name == name_hookah}.first.id if name_hookah != nil && id == nil
+    (tmp << stock.arr_hookahs.select {|i| i.id == id}).flatten!
+    (reserved_hookahs << stock.arr_hookahs.select {|i| i.id == id}).flatten!
+    stock.arr_hookahs.delete_if {|i| i.id == id}
+    reserved_hookahs.select {|i| i.id == id}.first.status = 'reserv'
+    tmp
+  end
+
+  def unreserv_hookah(reserved_hookahs, stock, id)
+    (stock.arr_hookahs << reserved_hookahs.select {|i| i.id == id}).flatten!
+    reserved_hookahs.delete_if {|i| i.id == id}
+    stock.arr_hookahs.select {|i| i.id == id}.first.status = 'free'
+  end
+
+  def reserv_bowl(reserved_bowls, stock, type_bowl = nil, id = nil)
+    tmp = []
+    if type_bowl == nil && id == nil
+      array = []
+      stock.arr_bowls.each {|i| array  << i.type}
+      type_max = array.each_with_object({}) {|item, memo| memo[item] = array.count(item)}.max_by{|k, v| v}.first
+      id = stock.arr_bowls.select {|i| i.type == type_max }.sample.id
+    end
+    id = stock.arr_bowls.select {|i| i.type == type_bowl}.first.id if type_bowl != nil && id == nil
+    (tmp = stock.arr_bowls.select {|i| i.id == id}).flatten!
+    (reserved_bowls << stock.arr_bowls.select {|i| i.id == id}).flatten!
+    stock.arr_bowls.delete_if {|i| i.id == id}
+    reserved_bowls.select {|i| i.id == id}.first.status = 'reserv'
+    tmp
+  end
+
+  def unreserv_bowl(reserved_bowls, stock, id)
+    (stock.arr_bowls << reserved_bowls.select {|i| i.id == id}).flatten!
+    reserved_bowls.delete_if {|i| i.id == id}
+    stock.arr_bowls.select {|i| i.id == id}.first.status = 'free'
+  end
+
+  def choice_tobaco(stock, name)
+    tmp = []
+    tmp = stock.arr_tobacco.select {|i| i.name == name}.first
+    stock.arr_tobacco.select {|i| i.name == name}.first.amount -= 20
+    stock.charcoals -= 8
+    puts "small balance #{stock.charcoals}" if stock.charcoals < 100
+    puts "small balance #{name}" if stock.arr_tobacco.select {|i| i.name == name}.first.amount < 60
+    tmp
+  end
 end
--- main.rb
+++ main.rb
@@ -11,68 +11,8 @@ def show_id(where, type)
   temp_arr_id
 end
 
-def reserv_table(arr_tables, id)
-  tmp = []
-  arr_tables.select {|elem| elem.id == id}.first.status = 'reserv'
-  tmp << arr_tables.select {|elem| elem.id == id}
-  puts "no more free table" if show_id(arr_tables, 'free').size == 0
-  tmp.flatten!
-end
-
-def unreserv_table(arr_tables, id)
-  arr_tables.select {|elem| elem.id == id}.first.status = 'free'
-end
-
 reserved_hookahs = []
-def reserv_hookah(reserved_hookahs, stock, name_hookah = nil, id = nil)
-  tmp = []
-  id = stock.arr_hookahs.sample.id if name_hookah == nil && id == nil
-  id = stock.arr_hookahs.select {|i| i.name == name_hookah}.first.id if name_hookah != nil && id == nil
-  (tmp << stock.arr_hookahs.select {|i| i.id == id}).flatten!
-  (reserved_hookahs << stock.arr_hookahs.select {|i| i.id == id}).flatten!
-  stock.arr_hookahs.delete_if {|i| i.id == id}
-  reserved_hookahs.select {|i| i.id == id}.first.status = 'reserv'
-  tmp
-end
-
-def unreserv_hookah(reserved_hookahs, stock, id)
-  (stock.arr_hookahs << reserved_hookahs.select {|i| i.id == id}).flatten!
-  reserved_hookahs.delete_if {|i| i.id == id}
-  stock.arr_hookahs.select {|i| i.id == id}.first.status = 'free'
-end
-
 reserved_bowls = []
-def reserv_bowl(reserved_bowls, stock, type_bowl = nil, id = nil)
-  tmp = []
-  if type_bowl == nil && id == nil
-    array = []
-    stock.arr_bowls.each {|i| array  << i.type}
-    type_max = array.each_with_object({}) {|item, memo| memo[item] = array.count(item)}.max_by{|k, v| v}.first
-    id = stock.arr_bowls.select {|i| i.type == type_max }.sample.id
-  end
-  id = stock.arr_bowls.select {|i| i.type == type_bowl}.first.id if type_bowl != nil && id == nil
-  (tmp = stock.arr_bowls.select {|i| i.id == id}).flatten!
-  (reserved_bowls << stock.arr_bowls.select {|i| i.id == id}).flatten!
-  stock.arr_bowls.delete_if {|i| i.id == id}
-  reserved_bowls.select {|i| i.id == id}.first.status = 'reserv'
-  tmp
-end
-
-def unreserv_bowl(reserved_bowls, stock, id)
-  (stock.arr_bowls << reserved_bowls.select {|i| i.id == id}).flatten!
-  reserved_bowls.delete_if {|i| i.id == id}
-  stock.arr_bowls.select {|i| i.id == id}.first.status = 'free'
-end
-
-def choice_tobaco(stock, name)
-  tmp = []
-  tmp = stock.arr_tobacco.select {|i| i.name == name}.first
-  stock.arr_tobacco.select {|i| i.name == name}.first.amount -= 20
-  stock.charcoals -= 8
-  puts "small balance #{stock.charcoals}" if stock.charcoals < 100
-  puts "small balance #{name}" if stock.arr_tobacco.select {|i| i.name == name}.first.amount < 60
-  tmp
-end
 
 stock = Stock.new
 stock.tables(1, 2, 'free')
@@ -110,10 +50,10 @@ puts "amount of charcoals - #{stock.charcoals}"
 
 puts '_______________________________'
 order1 = Order.new(1)
-order1.table = reserv_table(stock.arr_tables, 5)
-order1.hookah << reserv_hookah(reserved_hookahs, stock, nil, 3)
-order1.bowl << reserv_bowl(reserved_bowls, stock)
-order1.tobacco << choice_tobaco(stock, 'Dark_Side')
+order1.table =order1.reserv_table(stock.arr_tables, 5)
+order1.hookah << order1.reserv_hookah(reserved_hookahs, stock, nil, 3)
+order1.bowl << order1.reserv_bowl(reserved_bowls, stock)
+order1.tobacco << order1.choice_tobaco(stock, 'Dark_Side')
 
 puts "##{order1.order_number}\ntable ##{order1.table.first.id}\n"+
          "Hookah - #{order1.hookah.flatten.first.name}\nBowl - #{order1.bowl.flatten.first.type}\n"+
